fd4ae8b01104f5f087138c3a8867325d6d25a5a3
WILLIAM SOUEF <williamsouef@gmail.com>
2023-01-12 11:25:01 +0100

    Initial Commit

---
+++ CoreDataIntroduction/CoreDataIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataIntroductionApp.swift
+//  CoreDataIntroduction
+//
+//  Created by William Souef on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
